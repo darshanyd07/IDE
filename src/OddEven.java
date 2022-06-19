3b6dd58d25201e9ebbf2feeb6c18e0eaa56eaf97
champ09D <darshanmhatre709@gmail.com>
2022-06-19 08:59:45 +0530

    IDE Demo Add Successfully

---
+++ src/OddEven.java
@@ -0,0 +1,14 @@
+public class OddEven
+{
+    public static void main(String[] args) {
+        int num = 22;
+        if(num%2==0)
+        {
+            System.out.println("Even "+num);
+        }
+        else {
+            System.out.println("Odd "+num);
+        }
+    }
+
+}
